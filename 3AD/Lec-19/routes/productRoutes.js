420775d316297c9391a32f67a89ffeae7143c6df
shiva-verma <jztshiva@gmail.com>
2026-03-17 16:20:11 +0530

    updata and single product

---
+++ 3AD/Lec-19/routes/productRoutes.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const ProductModel = require("../models/productmodel")
+const { showAllProducts, showAddProductForm, createProduct } = require("../controllers/productController");
+const router = express.Router();
+
+router.get("/products", showAllProducts)
+
+router.get("/addproduct", showAddProductForm)
+
+router.post("/product/productdetail", createProduct)
+
+router.get("/singleproduct/:pid", async(req, res)=>{
+    try {
+        const productId = req.params.pid;
+        const product = await ProductModel.findById(productId)
+        res.render("singleProduct", {product})
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+router.get("/edit/:pid", async(req, res)=>{
+    try {
+        const productId = req.params.pid;
+        const product = await ProductModel.findById(productId);
+        res.render("updateProduct", {product});
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+router.patch("/productdetail/:pid", async(req, res)=>{
+    try {
+        const productId = req.params.pid;
+        const updatedData =  req.body;
+        await ProductModel.findByIdAndUpdate(productId, updatedData)
+        res.redirect("/products");  
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+module.exports = router
