60f25c44cbd72596bd8f48a7aaa325079d06cd9d
shiva-verma <jztshiva@gmail.com>
2026-04-13 10:42:49 +0530

    reviews

---
+++ 3AD/Lec-26/routes/productRoutes.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const ProductModel = require("../models/productmodel")
+const { showAllProducts, showAddProductForm, createProduct } = require("../controllers/productController");
+const { isSeller, isLoggedIn } = require("../middleware/authentication");
+const Review = require("../models/reviewmodel");
+const Product = require("../models/productmodel");
+const router = express.Router();
+
+router.get("/products", showAllProducts)
+
+router.get("/addproduct",isLoggedIn, isSeller ,showAddProductForm)
+
+router.post("/product/productdetail", isLoggedIn, isSeller, createProduct)
+
+router.get("/singleproduct/:pid", isLoggedIn ,async(req, res)=>{
+    try {
+        const productId = req.params.pid;
+        const product = await ProductModel.findById(productId).populate("reviews");
+        res.render("Products/singleProduct", {product})
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+router.get("/edit/:pid",isLoggedIn, isSeller ,async(req, res)=>{
+    try {
+        const productId = req.params.pid;
+        const product = await ProductModel.findById(productId);
+        res.render("Products/updateProduct", {product});
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+router.patch("/productdetail/:pid",isLoggedIn, isSeller,async(req, res)=>{
+    try {
+        const productId = req.params.pid;
+        const updatedData =  req.body;
+        await ProductModel.findByIdAndUpdate(productId, updatedData)
+        res.redirect("/products");  
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+router.delete("/product/:pid",isLoggedIn, isSeller, async(req, res)=>{
+  try {
+    const productId = req.params.pid;
+    await ProductModel.findByIdAndDelete(productId);
+    res.redirect("/products")
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+
+///reviews
+router.post("/review/:pid", async(req, res)=>{
+    const productId = req.params.pid;
+    const product = await Product.findById(productId)
+
+    const reviewId = await Review.create(req.body);
+
+    product.reviews.push(reviewId);
+
+    await product.save();
+    res.redirect(`/singleproduct/${productId}`)
+})
+
+module.exports = router
