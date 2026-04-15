7ca2455baa72811f8e1a93aebb632ffd94f2f338
shiva-verma <jztshiva@gmail.com>
2026-04-15 15:43:43 +0530

    middleware for author

--- 3X/Lec-32/middleware/authenticate.js
+++ 3X/Lec-32/middleware/authenticate.js
@@ -1,3 +1,5 @@
+const Review = require("../model/reviewSchema");
+
 const isLoggedIn = (req, res, next)=>{
     if(!req.isAuthenticated()){
         res.redirect("/login")
@@ -19,8 +21,18 @@ const isBuyer = (req, res, next)=>{
     res.redirect("/products")
 }
 
-const isAuthorReview = (req, res, next)=>{
+const isAuthorReview = async(req, res, next)=>{
+    //extract reviewId from params
+    const reviewId = req.params.rid;
+    
+    //find review using reviewid
+    const review = await Review.findById(reviewId)
 
+    //if review author and current logged in user are same then call next() function
+    if(review.author == req.user._id){
+        return next();
+    }
+  return res.send("you are not authorized")
 }
 
 module.exports = {isLoggedIn, isSeller, isBuyer, isAuthorReview}
